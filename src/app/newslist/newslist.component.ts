27b1774477e06fe78c381085b6dafea7aab84fc5
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-04-16 01:57:11 +0300

    configuring for heroku

--- src/app/newslist/newslist.component.ts
+++ src/app/newslist/newslist.component.ts
@@ -22,7 +22,7 @@ export class NewslistComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     // TODO: need to set config variables here
-    this.subscription = this.http.get<News>('https://golden-stage.herokuapp.com/api/news').subscribe(news => {
+    this.subscription = this.http.get<News>('https://guarded-shelf-22289.herokuapp.com/api/news').subscribe(news => {
       this.news = news;
     })
   }
