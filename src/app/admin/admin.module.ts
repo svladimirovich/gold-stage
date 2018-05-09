fb27dcce3f31bd1b47e831623222efe4d4629ec0
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-05-09 13:42:45 +0300

    added ReactiveFormsModule to admin/login component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { LoginComponent } from './login/login.component';
@@ -7,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AdminRoutingModule
   ],
   declarations: [LoginComponent]
