1e4a8aa2176f22976349a7038fb7cacbf6acb40d
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2018-05-12 19:39:52 +0300

    initialNavigation: "enabled" - gets rid of flicker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes)        
+        RouterModule.forRoot(routes, { initialNavigation: "enabled" })        
     ],
     exports: [RouterModule]
 })
